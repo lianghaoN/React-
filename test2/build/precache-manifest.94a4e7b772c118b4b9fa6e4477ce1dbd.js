9277038666e01da48dba5fd7f009e9c43f0e8142
lianghaoN <597868880@qq.com>
2019-11-07 10:40:00 +0800

    test2

---
+++ test2/build/precache-manifest.94a4e7b772c118b4b9fa6e4477ce1dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79729fffe1c5f4045fd9561b7f625cf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e7068bbfd6bad80f892",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "658a152535419a5959ff",
+    "url": "/static/css/main.4541d02c.chunk.css"
+  },
+  {
+    "revision": "7e7068bbfd6bad80f892",
+    "url": "/static/js/2.c235504f.chunk.js"
+  },
+  {
+    "revision": "658a152535419a5959ff",
+    "url": "/static/js/main.b2fa86bd.chunk.js"
+  },
+  {
+    "revision": "54b869a55364e235be08",
+    "url": "/static/js/runtime-main.877065d2.js"
+  }
+]);
